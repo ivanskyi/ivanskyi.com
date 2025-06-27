8542cd8ce7aa5a5f3a6154d98a603e1a7971ae15
ivanskyi <olehfromukraine@gmail.com>
2025-06-27 20:43:32 +0300

    Replaced the project using Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from "./component/about/about.component";
+import {HomeComponent} from "./component/home/home.component";
+import {PortfolioComponent} from "./component/portfolio/portfolio.component";
+import {ContactComponent} from "./component/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
